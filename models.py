689d9852d67af1dc990043cd86fd0e7859173087
fimhub <fredim.oxford@gmail.com>
2018-12-11 18:45:45 -0500

    updated to models

--- models.py
+++ models.py
@@ -8,70 +8,67 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_login import UserMixin
 
 
 db = SQLAlchemy()
 
+
+likes = db.Table('likes',
+    db.Column('lid', db.Integer),
+    db.Column('liker', db.Integer, db.ForeignKey('users.uid')),
+    db.Column('liked', db.Integer, db.ForeignKey('condos.cid'))
+)
+
 # Create class User
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(128), nullable=False)
+    likes = db.relationship(
+        'Condo',
+        secondary=likes,
+        lazy='dynamic'
+    )
+    def like(self, condo):
+        if not self.is_favorite(condo):
+            self.likes.append(condo)
+            db.session.commit()
+    def unlike(self, condo):
+        if self.is_favorite(condo):
+            self.likes.remove(condo)
+            db.session.commit()
+    def is_favorite(self, condo):
+        return self.likes.filter(
+            likes.c.liked == condo.cid
+        ).count() > 0
 
-    def __init__(self, username, password):
-    	self.username = username
-    	self.password = generate_password_hash(password)
-
-    def check_password(self,password):
-    	return check_password_hash(self.password,password)
-
-    def followed_posts(self):
-    	followed_posts = Condo.query.join(likes,(likes.c.following == Condo.mlsnum)).filter(likes.c.follower == self.id)
-    	return followed_posts
 
 class Condo(db.Model):
 #Create the relationship to the User table
 #users = db.relationship(User)
-		__tablename__ = 'condos'
-		#Create the relationship to the User table
-		#users = db.relationship(User)
-	  # Model for the Condo on the site.
-		id = db.Column(db.Integer, autoincrement=True)
-		mlsnum = db.Column(db.Integer, primary_key=True, autoincrement=False)
-		list_price = db.Column(db.Integer, nullable=False)
-		zip = db.Column(db.Integer, nullable=False)
-		sqft = db.Column(db.Integer, nullable=False)
-		beds = db.Column(db.Integer, nullable=False)
-		baths = db.Column(db.Integer, nullable=False)
-		photourl = db.Column(db.String, nullable=False)
-    #username = db.Column(db.String, db.ForeignKey('users.username'), nullable=False)
+    __tablename__ = 'condos'# Model for the Filter on the site.
+    cid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    mlsnum = db.Column(db.Integer, nullable=False)
+    zip = db.Column(db.String, nullable=False)
+    beds = db.Column(db.Integer, nullable=False)
+    baths = db.Column(db.Float, nullable=False)
+    sqft = db.Column(db.Float, nullable=False)
+    listprice = db.Column(db.Float, nullable=False)
+    photourl = db.Column(db.String, nullable=False)
 
 
-		def __init__(self, id):
-			self.id = id
+    def __init__(self, id):
+    	self.id = id
 
-		def __repr__(self):
-			return "Filter Id: {}".format(self.id)
+    def __repr__(self):
+    	return "Filter Id: {}".format(self.id)
 
-		def is_following(self, condo):
-			return self.followed.filter(likes.c.following == condo.mlsnum).count() > 0
 
-		def follow(self, condo):
-			if not self.is_following(condo):
-				self.followed.append(condo)
-
-		def unfollow(self, condo):
-			if self.is_following(condo):
-				self.followed.remove(condo)
-
-class Like(db.Model):
-	#Create the relationship to the User table
-	#users = db.relationship(User)
-
-	__tablename__ = 'likes'
-	fid = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	follower = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-	following = db.Column(db.Integer, db.ForeignKey('condos.mlsnum'), nullable=False)
-
+class Photo(db.Model):
+    __tablename__ = 'photos'
+    pid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    mlsnum = db.Column(db.Integer, nullable=False)
+    imgnum = db.Column(db.Integer, nullable=False)
+    features = db.Column(db.String, nullable=False)
